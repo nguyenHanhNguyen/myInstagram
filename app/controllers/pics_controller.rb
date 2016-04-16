b33ec77eca3cc06d09518640349e2d7880e49cf8
nguyenHanhNguyen <nhn22801@gmail.com>
2016-04-16 23:37:14 +0700

    add, edit, delete

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,8 +1,16 @@
 class PicsController < ApplicationController
 
+  # use the method find_pic for those following methods
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pics = Pic.all.order("created_at DESC")
+  end
+
+  def show
 
   end
+
 
   def new
     @pic = Pic.new
@@ -10,20 +18,45 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "It was posted"
+    else
+      render 'new'
+    end
   end
 
-  def show
-
-  end
 
   def destroy
 
+  end
+
+  def edit
+
+  end
+
+  # do not have view file, update the db
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Congrats, Pic was updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
   end
 
   private
 
   def pic_params
     params.require(:pic).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 
 end
